3b925311f2608c11b8c228541abb7ec779349be9
krishnavamsi886 <vamsi.nalajala07@gmail.com>
2019-12-27 08:51:17 -0500

    First commit in GIT

---
+++ HelloWorld/src/MyFirstClass.java
@@ -0,0 +1,17 @@
+
+public class MyFirstClass {
+
+	public static void main(String[] args) 
+	{
+		int i;
+		
+		for (i= 10;i<=12;i++)
+		{
+			
+		}
+		System.out.println("Hello World");
+		
+	//	System.out.println("Jain sidd"+i);		
+	}
+
+}
